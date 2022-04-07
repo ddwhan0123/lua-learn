41e3c6f49edb4b4e24fa05c73889653a36a74b39
Ezreal <526357367@qq.com>
2022-04-07 15:03:23 +0800

    feat:完成 for循环

--- src/forORWhile.lua
+++ src/forORWhile.lua
@@ -1,1 +1,14 @@
 --循环 例子
+flag =0
+while flag<10 do
+    flag=flag+1
+    if flag%2==0 then
+        print('while循环 ',flag)
+    end
+end
+
+-- 第一个参数 是起点 第二个参数是结果 第三个参数是执行次数(可不穿)
+for k = 0,10 do
+    print('for循环 ',k)
+end
+
