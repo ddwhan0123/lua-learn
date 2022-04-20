320fe198616004752f0fffa251a12b0ff9480a7e
Ezreal <526357367@qq.com>
2022-04-20 19:43:27 +0800

    update string sample

--- src/operator.lua
+++ src/operator.lua
@@ -48,10 +48,24 @@ else
 end
 
 -- ..字符串拼接
-a='hellow '
+a='heLLow '
 b='world'
 print(a..b)
 
 -- # 一元运算符
 print('打印 hellow的长度 ',#a)
 
+--一系列转意符
+print('000这是换行\n之后的字')
+--字符串操作
+print('upper全大写 方法 ',string.upper(a))
+print('lower全大写 方法 ',string.lower(a))
+a,b=string.gsub('aa111aaa',"a","o")
+print(a,b)--mainString 为要操作的字符串， findString 为被替换的字符，replaceString 要替换的字符，num 替换次数（可以忽略，则全部替换）
+c='hellow world'
+print(string.find(c,'ll'))--在一个指定的目标字符串 str 中搜索指定的内容 substr，如果找到了一个匹配的子串，就会返回这个子串的起始索引和结束索引，不存在则返回 nil
+print('内容反转',string.reverse(c))
+print(string.format('格式化拼接:%d',10086))
+print('字符串长度 ',string.len(c))
+print('内容拷贝 ',string.rep(c,3))--返回字符串string的n个拷贝
+--字符串截取
